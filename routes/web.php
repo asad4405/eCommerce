7d7391bbdb4d084094743381ae5140e69284c73a
asad4405 <asadrifat450@gmail.com>
2023-08-15 20:39:36 +0600

    add

--- routes/web.php
+++ routes/web.php
@@ -29,17 +29,19 @@ Route::post('/contact/post',[FrontendController::class, 'contact_post'])->name('
 Route::get('/dashboard',[HomeController::class ,'dashboard'])->name('dashboard')->middleware(['auth', 'verified']);
 Route::get('/vendor/approve/{id}',[HomeController::class ,'vendor_appreve'])->name('vendor.approve')->middleware(['auth', 'verified']);
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth','verified'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::post('/change/profile/photo', [ProfileController::class, 'change_profile_photo'])->name('change.profile.photo');
-});
 
-// category
-Route::resource('category',CategoryController::class);
-Route::get('category/restore/{id}',[CategoryController::class,'category_restore'])->name('category.restore');
-Route::get('category/delete/{id}',[CategoryController::class,'category_delete'])->name('category.delete');
+    // category
+    Route::resource('category',CategoryController::class)->middleware('admin.checker');
+    Route::get('category/restore/{id}',[CategoryController::class,'category_restore'])->name('category.restore')->middleware('admin.checker');
+    Route::get('category/delete/{id}',[CategoryController::class,'category_delete'])->name('category.delete')->middleware('admin.checker');
+
+    // product
+});
 
 // vendor
 Route::get('/vendor/register',[VendorController::class, 'vendor_register'])->name('vendor.register');
